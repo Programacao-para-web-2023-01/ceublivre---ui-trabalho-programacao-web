0a0a82696bb22494b891cd20bfdf8b82c971545b
Matheus <matheusalencar99@gmail.com>
2023-06-29 00:21:52 -0300

    add more screens

--- ceublivre-ui/src/app/information/[id]/page.tsx
+++ ceublivre-ui/src/app/information/[id]/page.tsx
@@ -1,71 +1,111 @@
-import styles from './styles.module.css'
+'use client'
+import './styles.css'
+import { useRouter } from 'next/navigation'
+
+
 
 export interface User {
-    id: number,
-    name: string
+    id: number
+    cpf: string
+    data_nascimento: string
+    preferencia_comunicacao: string
+    telefone: string
+    created_at: string
     email: string
-    phone: string
+    nome_completo: string
+    genero: string
+    senha: string
+    cep: string
+    endereco: string
+    updated_at: string
 }
-async function getUsers(id: number): Promise<User> {
-    const users = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-        .then((response) => response.json())
+
+
+async function getUsers(id: string): Promise<User> {
+    
+    const response = await fetch(`https://ceublivreapi-1-n3530025.deta.app/user?userId=${id}`)    
+    const users: User = await response.json();
+    console.log(users.id)
     return users
 }
 export default async function Information({params}: {
     params: {
-        id: number
+        id: string
     }
 
-}) {
-
-
-
-    /*
-    const [user, setUser] = useState<User>()
-
-    useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
-            .then((response) => response.json())
-            .then((response) => {
-                console.log(response)
-                setUser(response)
-            } )
-
-    }, [params.id])
-
-     */
-
+}) 
+    
+{
+    const router = useRouter()
     const user = await getUsers(params.id)
 
-    return (
-        <form action="/send-data-here" method="post">
-            <label htmlFor="name">Nome</label>
-            <input type="text" id="name" name="first" value={user?.name}/>
+        return (
+        <form action="/test" method="POST">
+            <h1>Dados Pessoais</h1>
+            <fieldset>
+                   <legend>
+                       <h2>Dados do usuário</h2>
+                   </legend>
+                <div className="field-group">
+                   <div className="field">
+                           <label htmlFor="name">Id usuário</label>
+                           <input type="text" name="id" required value={user?.id}/>
+                    </div>
+                       <div className="field">
+                           <label htmlFor="name">Nome Completo</label>
+                           <input type="text" name="name" required value={user?.nome_completo}/>   
+                       </div>
+                    </div>
+                       <div className="field-group">
+                           <div className="field">
+                               <label htmlFor="genero">Genero</label>
+                               <input type="text" name="genero" value={user?.genero} required/>
+                           </div>
+                           <div className="field">
+                                   <label htmlFor="data_nascimento">Data nascimento</label>
+                                   <input type="text" name="data_nascimento" required value={user?.data_nascimento}/>
+                               </div>
+                        </div>
 
+                       <div className="field-group">
+                           <div className="field">
+                               <label htmlFor="email">Email</label>
+                               <input type="text" name="email" value={user?.email}/>
+                           </div>
+                           <div className="field">
+                               <label htmlFor="cpf">CPF</label>
+                               <input type="text" name="cpf" value={user?.cpf}/>
+                           </div>
+                        </div>
+                        <div className="field-group">
+                           <div className="field">
+                               <label htmlFor="telefone">Telefone</label>
+                               <input type="text" name="telefone" value={user?.telefone}/>
+                           </div>
+                           <div className="field">
+                               <label htmlFor="preferencia_comunicacao">Preferencia comunicacao</label>
+                               <input type="text" name="preferencia_comunicacao" value={user?.preferencia_comunicacao}/>
+                           </div>
+                        </div>
+                        <div className="field-group">
+                           <div className="field">
+                               <label htmlFor="cep">CEP</label>
+                               <input type="text" name="cep" value={user?.cep}/>
+                           </div>
+                           <div className="field">
+                               <label htmlFor="endereco">Endereco</label>
+                               <input type="text" name="endereco" value={user?.endereco}/>
+                           </div>
+                        </div>
 
-            <label htmlFor="gender">Sexo</label>
-            <input type="text" id="gender" name="gender" value={"M"}/>
-
-            <label htmlFor="data">Data nascimento</label>
-            <input type="date" id="date" name="date" value={"1999-07-24"}/>
-
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" value={user?.email}/>
-
-            <label htmlFor="cpf">Cpf: </label>
-            <input type="text" id="cpf" name="cpf" value={"3344dddd"}/>
-
-            <label htmlFor="phone">Telefone:</label>
-            <input type="text" id="phone" name="phone" value={user?.phone}/>
-
-            <label htmlFor="cep">Cep:</label>
-            <input type="text" id="cep" name="cep" value={"xxxdddfff"}/>
-
-            <label htmlFor="address">Endereco:</label>
-            <input type="text" id="address" name="address" value={" rua rua 89 lote 223"}/>
-
-            <button type="submit">Submit</button>
-        </form>
-
-    )
+               </fieldset>
+               <button type="submit" style={{marginRight: 10}}>
+                Editar Perfil
+               </button>
+               <button onClick={() => router.push('/')} style={{backgroundColor:'orange'}}>
+                Voltar pagina inicial
+               </button>
+            </form>
+           )
+    
 }
--- ceublivre-ui/src/app/purchases/page.tsx
+++ ceublivre-ui/src/app/purchases/page.tsx
@@ -1,5 +1,66 @@
-export default function Purchases() {
-    return (
-        <h1>Purchase information!!!</h1>
-    )
+import './styles.css'
+import ChangePassword from '../change_password/page'
+export default function PurchaseInformation({params}: {
+    params: {
+        id: string
+    }
+
+}) {
+        return (
+        <>
+        <form action="/test" method="POST">
+            <h1>Histórico de pedidos</h1>
+            <fieldset>
+                   <legend>
+                       <h2>Dados dos pedidos</h2>
+                   </legend>
+                <div className="field-group">
+                   <div className="field">
+                           <label htmlFor="id">Id pedido</label>
+                           <input type="text" name="id" required value={"teste"}/>
+                    </div>
+                       <div className="field">
+                           <label htmlFor="endereco">Endereço de entrega</label>
+                           <input type="text" name="endereco" value={"teste"}/>   
+                       </div>
+                    </div>
+                       <div className="field-group">
+                           <div className="field">
+                               <label htmlFor="frete">Frete</label>
+                               <input type="text" name="frete" value={"teste"} required/>
+                           </div>
+                           <div className="field">
+                                   <label htmlFor="valor">Valor Total</label>
+                                   <input type="text" name="valor" required value={"teste"}/>
+                               </div>
+                        </div>
+
+                       <div className="field-group">
+                           <div className="field">
+                               <label htmlFor="status">Status</label>
+                               <input type="text" name="status" value={"teste"}/>
+                           </div>
+                           <div className="field">
+                               <label htmlFor="criado_em">Criado</label>
+                               <input type="text" name="criado_em" value={"teste"}/>
+                           </div>
+                        </div>
+                        <div className="field-group">
+                           <div className="field">
+                               <label htmlFor="produto">Produto</label>
+                               <input type="text" name="produto" value={"teste"}/>
+                           </div>
+                         </div>
+
+               </fieldset>
+               <button type="submit" style={{marginRight: 10}}>
+                Visualizar Perfil
+               </button>
+               <button type="submit">
+               Alterar senha
+               </button>
+            </form>
+            </>
+           )
+    
 }
